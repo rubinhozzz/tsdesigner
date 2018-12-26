8c548b88b1e33e92c4151ede44b48958c5048256
rubinhozzz <ruben.flex@gmail.com>
2018-12-26 12:53:10 +0100

    text added to tshirt

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
 var canvas = new fabric.Canvas('c');
+var t = new fabric.Text('hello world', { left: 400, top: 300 });
 
 fabric.Image.fromURL('images/front.png', function(img) {
-    img.scale(0.5).set({
-        left: 100,
+    img.scale(1).set({
+        left: 200,
         top: 100,
+
         //angle: -15,
         //clipTo: function (ctx) {
         //    ctx.arc(0, 0, radius, 0, Math.PI * 2, true);
         //}
     });
-    canvas.add(img).setActiveObject(img);
+    canvas.add(img);
+    canvas.add(t).setActiveObject(t);
+    canvas.bringToFront(t);
 });
 
-// create a rectangle object
-var rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: 'red',
-    width: 100,
-    height: 100
-});
 
-// "add" rectangle onto canvas
-canvas.add(rect);
+
+document.getElementById('btn_front').addEventListener('click', function(evt) {
+    let txt= document.getElementById('fronttext').value;
+    t.text = txt;
+    canvas.renderAll();
+}, false);
 
 }, false)
